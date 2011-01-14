134b014aaa3af6f16f9610ecff386a3ef6e96669
Techno <Techno@76c3dd84-03ae-47c5-963a-2991f67f3736>
2011-01-14 00:01:33 +0000

    Added log4j, convenience methods

--- src/org/ua2/clientlib/UAConnection.java
+++ src/org/ua2/clientlib/UAConnection.java
@@ -6,17 +6,22 @@ import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.ua2.clientlib.exception.NoConnectionError;
 import org.ua2.edf.EDFData;
 import org.ua2.edf.parser.EDFParser;
+import org.ua2.edf.parser.ParseException;
+import org.ua2.edf.parser.TokenMgrError;
 
 /**
  * A UAConnection manages an individual user's connection to the UA server
  * <p>
- * It deals with sending and receiving EDF, but not session management 
+ * It deals with sending and receiving EDF, but not session management
+ * 
  * @author brian
- *
+ * 
  */
 public class UAConnection implements Runnable {
 
@@ -25,228 +30,248 @@ public class UAConnection implements Runnable {
 	private DataOutputStream output;
 	private EDFParser inputparser;
 
-	public enum ConnectionStatus { NOTCONNECTED, CONNECTFAILED, CONNECTED, LOSTCONNECTION };
+	public enum ConnectionStatus {
+		NOTCONNECTED, CONNECTFAILED, CONNECTED, LOSTCONNECTION
+	};
 
 	public ConnectionStatus status = ConnectionStatus.NOTCONNECTED;
 	public String statusmessage = "Not connected";
-	
+
 	// SynchronousQueue is a "queue" which only allows one item on it
 	// It blocks on write until something comes along and reads it, and
 	// blocks on read until something writes to it.
 	private SynchronousQueue<EDFData> reply = new SynchronousQueue<EDFData>();
 
 	private AnnounceQueue announcequeue = new AnnounceQueue();
-	
+
+	private static final Logger logger = Logger.getLogger(UAConnection.class);
+
 	/**
-	 * This code is run in its own thread, started when the UA server
-	 * connection is established
+	 * This code is run in its own thread, started when the UA server connection
+	 * is established
 	 * <p>
-	 * It loops forever (or until there's an error), calling readEDF (which blocks
-	 * until an EDF tree is available from the parser), and, depending on the EDF
-	 * message, either makes it available as a reply (presumed to be in response to
-	 * a request) or pushes it onto the object's announcement FIFO.
+	 * It loops forever (or until there's an error), calling readEDF (which
+	 * blocks until an EDF tree is available from the parser), and, depending on
+	 * the EDF message, either makes it available as a reply (presumed to be in
+	 * response to a request) or pushes it onto the object's announcement FIFO.
 	 */
-	public void run()
-	{
-		EDFData inputedf;
-		
-		// Loop round parsing the input
-		while(true)
-		{
-			inputedf = readEDF();		// FIXME - no null handling
+	public void run() {
+		try {
+			EDFData inputedf;
 
-			/* EDF from the server can be one of three things
-			 * 	edf - response to connection message <edf="on"/>. Not used here
-			 * 	reply - reply to a request
-			 * 	announce - unsolicited announcement
-			 */
-
-			if(inputedf.name.equals("announce"))
-			{
-				// Pass to the announcement handler
-				announcequeue.announce(inputedf);
-				inputedf.print();
-			}
-			else if(inputedf.name.equals("reply"))
-			{
-				try
-				{
-					inputedf.print();
-
-					// FIXME - needs a timeout.
-					reply.put(inputedf);
+			// Loop round parsing the input
+			while (true) {
+				logger.trace("Status " + status);
+				inputedf = readEDF();
+				if (inputedf == null) {
+					break;
 				}
-				catch(InterruptedException e)
-				{
-					// Something went wrong
-					setStatus(ConnectionStatus.NOTCONNECTED, "Internal error in server: " + e.getLocalizedMessage());
-					closeConnection();
-					
-					System.err.println("Parser thread interrupted on put(): " + e.getLocalizedMessage());
-					e.printStackTrace();
-					
-					// 
-					// Exit this thread (Thread.currentThread().stop() is deprecated)
-					return;
+
+				/*
+				 * EDF from the server can be one of three things edf - response
+				 * to connection message <edf="on"/>. Not used here reply -
+				 * reply to a request announce - unsolicited announcement
+				 */
+
+				if (inputedf.name.equals("announce")) {
+					// Pass to the announcement handler
+					announcequeue.announce(inputedf);
+					if (logger.isTraceEnabled()) logger.trace("Announcement:\n" + inputedf.format(true));
+				} else if (inputedf.name.equals("reply")) {
+					try {
+						if (logger.isTraceEnabled())
+							logger.trace("Reply:\n" + inputedf.format(true));
+
+						// FIXME - needs a timeout.
+						reply.put(inputedf);
+					} catch (Exception e) {
+						// Something went wrong
+						setStatus(
+								ConnectionStatus.NOTCONNECTED,
+								"Internal error in server: "
+										+ e.getLocalizedMessage());
+						closeConnection();
+
+						logger.error("Parser thread interrupted on put", e);
+
+						//
+						// Exit this thread (Thread.currentThread().stop() is
+						// deprecated)
+						return;
+					}
+				} else {
+					// FIXME - what should we do with unknown EDF messages?
+					logger.error("Unknown EDF response:\n"
+							+ inputedf.format(true));
 				}
 			}
-			else
-			{
-				// FIXME - what should we do with unknown EDF messages?
-				System.out.println("Unknown EDF response");
-				inputedf.print();
-			}
+			//
+			// This point not reached
+		} catch (Exception e) {
+			logger.error("Stopped loop due to error", e);
 		}
-		//
-		// This point not reached
 	}
-	
+
 	/**
 	 * Sets the status and status message, but only if the status has changed
 	 * <p>
-	 * The first status message is usually the only useful one. Subsequent messages
-	 * are more likely to be 'null' and similar
-	 * @param st	ConnectionStatus enumerated status
-	 * @param message	message, eg. exception error message
+	 * The first status message is usually the only useful one. Subsequent
+	 * messages are more likely to be 'null' and similar
+	 * 
+	 * @param st
+	 *            ConnectionStatus enumerated status
+	 * @param message
+	 *            message, eg. exception error message
 	 */
-	private void setStatus(ConnectionStatus st, String message)
-	{
-		if (st != status)
-		{
+	private void setStatus(ConnectionStatus st, String message) {
+		if (st != status) {
 			status = st;
 			statusmessage = message;
 		}
 	}
-	
+
 	/**
-	 * Close this UAConnection's open network connection, if any. If 
-	 * an exception is thrown, it is ignored
+	 * Close this UAConnection's open network connection, if any. If an
+	 * exception is thrown, it is ignored
 	 */
-	private void closeConnection()
-	{
-		try
-		{
+	private void closeConnection() {
+		try {
 			// Close the connection, if it's not already gone away
 			connection.close();
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			// Ignore it
 		}
 	}
-	
+
 	/**
 	 * Connect to UA server
-	 * @param host	Hostname of UA server
-	 * @param port	Port of UA server
+	 * 
+	 * @param host
+	 *            Hostname of UA server
+	 * @param port
+	 *            Port of UA server
 	 */
-	public boolean connect(String host, int port) throws UnknownHostException, IOException
-	{
-		try
-		{
+	public boolean connect(String host, int port) throws UnknownHostException,
+			IOException {
+		try {
 			connection = new Socket(host, port);
-				
+
 			input = connection.getInputStream();
 			output = new java.io.DataOutputStream(connection.getOutputStream());
-	
+
 			inputparser = new org.ua2.edf.parser.EDFParser(input);
-			
+
 			sendEDF(new EDFData("edf", "on"));
-			
-			readEDF().print();		// FIXME - handle this properly
-			System.out.println("Status: " + statusmessage);
-			
+
+			logger.debug("EDF on response:\n" + readEDF().format(true)); // FIXME
+																			// -
+																			// handle
+																			// this
+																			// properly
+			logger.debug("Status: " + statusmessage);
+
 			// Fire up the reader thread
 			(new Thread(this)).start();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			setStatus(ConnectionStatus.CONNECTFAILED, e.getLocalizedMessage());
-			
+
 			return false;
 		}
-		
+
 		setStatus(ConnectionStatus.CONNECTED, "Connected to UA server");
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Sends an EDF tree to the server
-	 * @param edf	EDFData object to send
-	 * @return	true if the sending suceeded, false otherwise
+	 * 
+	 * @param edf
+	 *            EDFData object to send
+	 * @return true if the sending suceeded, false otherwise
 	 */
-	public boolean sendEDF(EDFData edf)
-	{
-		try
-		{
-			output.writeBytes(edf.toString());
-		}
-		catch (Exception e)
-		{
+	public boolean sendEDF(EDFData edf) {
+		try {
+			String str = edf.toString();
+			if (logger.isTraceEnabled())
+				logger.trace("Writing EDF:" + str);
+			output.writeBytes(str);
+		} catch (Exception e) {
 			// FIXME - is this the right connection status?
 			// FIXME - throw a NoConnectionError exception
 			setStatus(ConnectionStatus.CONNECTFAILED, e.getLocalizedMessage());
-			
+
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
+	private void handleError(Throwable t) {
+		// FIXME - is this the right connection status?
+		// FIXME - throw a NoConnectionError exception
+		setStatus(ConnectionStatus.CONNECTFAILED, t.getLocalizedMessage());
+
+		closeConnection();
+	}
+
 	/**
 	 * Reads an EDFData object from the server
-	 * @return	EDFData object, or null if there was an error
+	 * 
+	 * @return EDFData object, or null if there was an error
 	 */
-	public EDFData readEDF()
-	{
-		EDFData edf;
-		
-		try
-		{
+	public EDFData readEDF() {
+		EDFData edf = null;
+
+		try {
 			edf = inputparser.elementtree();
+			if (logger.isTraceEnabled()) logger.trace("Read EDF:\n" + edf.format(true));
+			return edf;
+		} catch (TokenMgrError e) {
+			logger.error("Cannot parse EDF", e);
+			handleError(e);
+		} catch (ParseException e) {
+			logger.error("Cannot parse EDF", e);
+			handleError(e);
+		} catch (Exception e) {
+			logger.error("Exception during EDF read", e);
+			handleError(e);
 		}
-		catch (Exception e)
-		{
-			// FIXME - is this the right connection status?
-			// FIXME - throw a NoConnectionError exception
-			setStatus(ConnectionStatus.CONNECTFAILED, e.getLocalizedMessage());
-			
-			closeConnection();
-			
-			return null;
-		}
-		
-		return edf;
+
+		return null;
 	}
-	
-	public EDFData sendAndRead(EDFData send) throws NoConnectionError
-	{
-		EDFData replymessage;
-		
+
+	public synchronized EDFData sendAndRead(EDFData send) throws NoConnectionError {
+		EDFData replymessage = null;
+
 		// Send data
+		if (logger.isTraceEnabled()) logger.trace("Sending:\n" + send.format(true));
 		sendEDF(send);
-		
+
 		// Get reply
-		try
-		{
-			// FIXME - needs a timeout
-			replymessage = reply.take();
+		try {
+			while (replymessage == null && status == ConnectionStatus.CONNECTED) {
+				replymessage = reply.poll(5, TimeUnit.SECONDS);
+			}
+		} catch (Exception e) {
+			logger.error("Internal error in server", e);
 		}
-		catch(InterruptedException e)
-		{
+
+		if (replymessage == null) {
 			// Something went wrong
-			setStatus(ConnectionStatus.NOTCONNECTED, "Internal error in server: " + e.getLocalizedMessage());
+			setStatus(ConnectionStatus.NOTCONNECTED, "No reply");
 			closeConnection();
-			
+
 			throw new NoConnectionError(NoConnectionError.Reason.CONNECTIONLOST);
 		}
 		
+		if(logger.isTraceEnabled()) logger.trace("Sent " + send.getString() + " got " + replymessage.getString());
+
+		if (logger.isTraceEnabled()) logger.trace("Read:\n" + replymessage.format(true));
 		return replymessage;
 	}
 
-	public AnnounceQueue getAnnounceQueue()
-	{
+	public AnnounceQueue getAnnounceQueue() {
 		return announcequeue;
 	}
 }
--- src/org/ua2/clientlib/UASession.java
+++ src/org/ua2/clientlib/UASession.java
@@ -1,5 +1,8 @@
 package org.ua2.clientlib;
 
+import java.net.InetAddress;
+
+import org.apache.log4j.Logger;
 import org.ua2.clientlib.exception.NoConnectionError;
 import org.ua2.edf.EDFData;
 
@@ -16,7 +19,12 @@ public class UASession
 	
 	private UA ua;
 	
-	private String clientname = "JEDF 0.1-dev";
+	private String clientname = "JEDF 0.2-dev";
+	private String version = "2.6-beta17";
+
+	private EDFData user = null;
+
+	private static final Logger logger = Logger.getLogger(UASession.class);
 	
 	/**
 	 * Create a new UASession, with the UA object set by the single static instance
@@ -57,7 +65,7 @@ public class UASession
 			sessionstatus = SessionStatus.CONNECTED;
 			
 			// FIXME - debug code
-			System.out.println("Connected");
+			logger.debug("Connected");
 		}
 		catch (Exception e)
 		{
@@ -73,37 +81,63 @@ public class UASession
 	 */
 	public void setClientName(String name)
 	{
-		clientname = name;
+		this.clientname = name;
 	}
+	
+	public void setClientProtocol(String version) {
+		this.version = version;
+	}
+	
+	public EDFData sendAndRead(EDFData sendData) throws NoConnectionError {
+		UAConnection connection = (UAConnection) ua.get(UAConnection.class);
+		return connection.sendAndRead(sendData);
+	}
+	
+	public int getUserId() {
+		if(user == null) {
+			return -1;
+		}
+		
+		return user.getChild("userid").getInteger();
+	}
+	
+	public boolean login(String username, String password) throws NoConnectionError {
+		return login(username, password, null, false);
+	}
+	
 	
 	/**
 	 * Logs in to the server
 	 * 
 	 * @param username	User's username
 	 * @param password	User's plain-text password
+	 * @param address 
 	 * @return	true if users is successfully logged in, false otherwise
 	 * @throws NoConnectionError	Connection to UA server has been lost or is otherwise unavailable
 	 */
-	public boolean login(String username, String password) throws NoConnectionError
+	public boolean login(String username, String password, InetAddress address, boolean shadow) throws NoConnectionError
 	{
 		// FIXME - need to handle multiple login attempts
 		
-		EDFData sendData;
-		EDFData readData;
-		
-		UAConnection connection = (UAConnection) ua.get(UAConnection.class);
-					
-		sendData = new EDFData("request", "user_login");
+		EDFData sendData = new EDFData("request", "user_login");
 		sendData.add("name", username);
 		sendData.add("password", password);
 		sendData.add("client", clientname);
-		sendData.add("protocol", "2.6-beta17");
-
-		readData = connection.sendAndRead(sendData);
+		sendData.add("protocol", version);
+		if(address != null) {
+			sendData.add("hostname", address.getHostAddress());
+			sendData.add("address", address.getHostAddress());
+		}
+		if(shadow) {
+			sendData.add("status", 256);
+		}
+		
+		EDFData readData = sendAndRead(sendData);
 		
 		if(readData.sValue.equals("user_login"))
 		{
 			// Login successful
+			user  = readData;
 			
 			sessionstatus = SessionStatus.LOGGEDIN;
 			
@@ -123,8 +157,7 @@ public class UASession
 	 */
 	public void logout() throws NoConnectionError
 	{
-		UAConnection connection = (UAConnection) ua.get(UAConnection.class);
-		connection.sendAndRead(new EDFData("request", "user_logout"));
+		sendAndRead(new EDFData("request", "user_logout"));
 	}
 
 	
@@ -138,27 +171,23 @@ public class UASession
 	{
 		// FIXME - this is not the best place for this method
 
-		System.out.println("Request for login banner");
+		logger.debug("Request for login banner");
 
-		UAConnection connection = (UAConnection) ua.get(UAConnection.class);
-		
 		String banner = "No banner";
 		
 		try
 		{
-			EDFData bannerresponse = connection.sendAndRead(new EDFData("request", "system_list"));
-			System.out.println("Status: " + connection.statusmessage);
+			EDFData bannerresponse = sendAndRead(new EDFData("request", "system_list"));
 
 			banner = bannerresponse.getChild("banner").sValue;
 		}
 		catch(Exception e)
 		{
-			System.out.println(e.toString());
+			logger.error("Cannot get banner", e);
 		}
 		
-		System.out.println("Returning banner");
+		logger.debug("Returning banner");
 		
 		return banner;
 	}
-	
 }
